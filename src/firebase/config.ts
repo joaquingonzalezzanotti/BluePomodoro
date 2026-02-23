6330cd5107c2951af3bbf1ee08ef31b8283c1236
Joaquin González <joaquingzzz79@gmail.com>
2026-02-23 19:21:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2606512430-8edee",
+  "appId": "1:860795644232:web:da5333cc20179673beab7d",
+  "apiKey": "AIzaSyCYeOrh0IUB_0iu3_j_9L3H2Xf4k6BNAho",
+  "authDomain": "studio-2606512430-8edee.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "860795644232"
+};
