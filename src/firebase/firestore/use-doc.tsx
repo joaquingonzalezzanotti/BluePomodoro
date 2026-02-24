a158f25d14f703023581e51679c465c9ceb0a2db
Joaquin González <joaquingzzz79@gmail.com>
2026-02-24 16:02:35 +0000

    React2Shell Security Bulletin CVE-2025-55182 is a critical vulnerability

--- src/firebase/firestore/use-doc.tsx
+++ src/firebase/firestore/use-doc.tsx
@@ -32,12 +32,6 @@ export interface UseDocResult<T> {
  * IMPORTANT! YOU MUST MEMOIZE the inputted memoizedTargetRefOrQuery or BAD THINGS WILL HAPPEN
  * use useMemo to memoize it per React guidence.  Also make sure that it's dependencies are stable
  * references
- *
- *
- * @template T Optional type for document data. Defaults to any.
- * @param {DocumentReference<DocumentData> | null | undefined} docRef -
- * The Firestore DocumentReference. Waits if null/undefined.
- * @returns {UseDocResult<T>} Object with data, isLoading, error.
  */
 export function useDoc<T = any>(
   memoizedDocRef: DocumentReference<DocumentData> | null | undefined,
@@ -100,11 +94,7 @@ export function useDoc<T = any>(
     );
 
     return () => unsubscribe();
-  }, [memoizedDocRef]); // Re-run if the memoizedDocRef changes.
-
-  if(memoizedDocRef && !(memoizedDocRef as any).__memo) {
-    console.warn('Reference passed to useDoc was not memoized with useMemoFirebase. This can cause infinite loops.');
-  }
+  }, [memoizedDocRef]);
 
   return { data, isLoading, error };
 }
